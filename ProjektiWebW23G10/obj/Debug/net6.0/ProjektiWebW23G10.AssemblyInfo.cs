d1b1f451b02d0b7580703f1853619fc1323c04c9
anjeza <anjezagllarevaa@gmail.com>
2023-12-10 23:24:52 +0100

    Add .gitignore

--- ProjektiWebW23G10/obj/Debug/net6.0/ProjektiWebW23G10.AssemblyInfo.cs
+++ ProjektiWebW23G10/obj/Debug/net6.0/ProjektiWebW23G10.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjektiWebW23G10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e02ed188df94ac0584f48f3ac556e5ddb80b8dc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjektiWebW23G10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjektiWebW23G10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
