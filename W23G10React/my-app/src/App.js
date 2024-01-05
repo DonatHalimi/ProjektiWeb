8cd0dc5500b4038f34dfe636cb4ad39594e025b1
DonatHalimi <118883706+DonatHalimi@users.noreply.github.com>
2024-01-05 20:44:09 +0100

    readded SingleProduct route

--- W23G10React/my-app/src/App.js
+++ W23G10React/my-app/src/App.js
@@ -4,7 +4,7 @@ import Index from './pages/Index';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Shop from './pages/Shop';
-//import SingleProduct from './pages/SingleProduct';
+import SingleProduct from './pages/SingleProduct';
 import Checkout from './pages/Checkout';
 import Cart from './pages/Cart';
 import Footer from './pages/Footer';
@@ -25,6 +25,8 @@ function App() {
           <ToastContainer />
           <ToTop />
           <Routes>
+            <Route path='/singleproduct' element={<SingleProduct/>}></Route>
+
             <Route path='/' element={<Index />}></Route>
             <Route path='/index' element={<Index />}></Route>
             <Route path='/about' element={<About />}></Route>
